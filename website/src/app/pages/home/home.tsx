aa73fe38433253cd8da9e83ce728bdb0e0551ffe
Siveing <siveing.huyy@gmail.com>
2024-01-05 17:21:15 +0700

    fix(): tsconfig fixed

--- website/src/app/pages/home/home.tsx
+++ website/src/app/pages/home/home.tsx
@@ -1,6 +1,6 @@
 import { sum } from "@mfe-demo-jin/libs";
 import Layout from "../../layouts/layout";
-import { CounterNumber, ShowHideAction } from "about/Module"
+import App, { CounterNumber, ShowHideAction } from "about/Module"
 
 function HomePage() {
     return (
@@ -9,13 +9,15 @@ function HomePage() {
             <p>Testing function <strong>sum</strong></p>
             <p>{sum(10, 10)}</p>
 
-            <hr style={{ margin: '20px 0' }} />
+            {/* <hr style={{ margin: '20px 0' }} /> */}
+            {/* <App /> */}
 
+            <hr style={{ margin: '20px 0' }} />
             <p className="mb-2">Getting the component from about</p>
             <CounterNumber />
 
             <hr style={{ margin: '20px 0' }} />
-            
+
             <p className="mb-2">Trigger action from host site to remote</p>
             <ShowHideAction />
         </Layout>
